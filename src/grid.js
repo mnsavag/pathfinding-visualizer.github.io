8f107f4989b33da1e94575f2bee7f61ce7dd0569
mnsavag <mnsavag@gmail.com>
2023-07-23 17:20:57 +0500

    reworked grid and added adaptive to it

--- src/grid.js
+++ src/grid.js
@@ -1,39 +1,53 @@
-const paleBlue = "rgb(179,188,187)";
-const smokyWhite = "rgb(238,236,236)";
+const xCellCnt = 64;
+const yCellCnt = 23;
+const grid = document.querySelector(".grid");
 
-const ctx = document.querySelector(".grid").getContext("2d");
-const LINE_TYPES = 3;
-const beginPos = [0.5, 50.5, 0.5];
-const step     = [25,  50,  50];
-const style    = [smokyWhite, paleBlue];
 
-window.addEventListener("resize", () => requestAnimationFrame(createGrid));
+window.addEventListener("resize", () => requestAnimationFrame(resizeGrid));
 
 
 function createGrid() {
-    ctx.canvas.width  = document.documentElement.clientWidth;
-    ctx.canvas.height = document.documentElement.clientHeight;
-    
-    for (let lineType = 0; lineType < LINE_TYPES; ++lineType){
-        for (let dir = 0; dir < 2; ++dir) {
-            ctx.beginPath()
-            ctx.strokeStyle = style[lineType];
-            let index = beginPos[lineType];
-            if (dir == 0)
-                while (index < ctx.canvas.width) {
-                    ctx.moveTo(index, 0);
-                    ctx.lineTo(index, ctx.canvas.height);
-                    index += step[lineType];
-                }
-            else
-                while (index < ctx.canvas.height) {
-                    ctx.moveTo(0, index);
-                    ctx.lineTo(ctx.canvas.width, index);
-                    index += step[lineType];
-                }
-            ctx.stroke();
+    resizeGrid()
+    for (let x = 0; x < yCellCnt; x++) {
+        const row = createDOMWithId("tr", x);
+        for (let y = 0; y < xCellCnt; y++) {
+            const cell = createDOMWithId("td", `${x} ${y}`);
+            row.appendChild(cell);
         }
+        grid.appendChild(row);
     }
+}
+
+function resizeGrid() {
+    const blockHeight = document.documentElement.clientHeight - 50 - 80; // change when will adaptive header, section
+    const blockWidth = document.documentElement.clientWidth;
+    const table = getTableObj(blockWidth, blockHeight);
+
+    grid.style.width = `${table.width}px`;
+    grid.style.height = `${table.height}px`;
+}
+
+function getTableObj(blockWidth, blockHeight) {
+    let gridWidth = blockWidth - blockWidth % xCellCnt;
+    let gridHeight = blockHeight - blockHeight % yCellCnt;
+
+    let cellWidth = gridWidth / xCellCnt;
+    let cellHeight = gridHeight / yCellCnt;
+    
+    gridWidth = cellWidth * xCellCnt - cellWidth;
+    gridHeight = cellHeight * yCellCnt - cellHeight;
+    return {
+        width: gridWidth,
+        height: gridHeight,
+    }
+}
+
+function createDOMWithId(tag, id) {
+    const domElem = document.createElement(tag);
+    let idAttr = document.createAttribute("id");
+    idAttr.value = id;
+    domElem.setAttributeNode(idAttr);
+    return domElem;
 }
 
 createGrid();
