fd4d94f4a7e7bf68f23009d060ffbabc3b42a106
mnsavag <mnsavag@gmail.com>
2023-08-09 00:49:10 +0500

    Change relative paths

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express()
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-app.use("/", express.static(__dirname + "/src"))
+app.use("/src", express.static(__dirname + "/src"))
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html")
 })
