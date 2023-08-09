b49d16f6efa6dd29b49f49d868a133bf2747c01b
mnsavag <mnsavag@gmail.com>
2023-08-09 14:37:26 +0500

    Change relative paths

--- src/DOMEntities/mapDOM.js
+++ src/DOMEntities/mapDOM.js
@@ -1,58 +1,58 @@
-import { getGreatestMultiple, getSelectorHeight } from "../miscellaneous/utility.js"
-import { createCellTag } from "./cellDOM.js"
+//import { getGreatestMultiple, getSelectorHeight } from "../miscellaneous/utility.js"
+//import { createCellTag } from "./cellDOM.js"
 
 
-export class MapDOM {
-    constructor(map) {
-        this.xCnt = map[0].length
-        this.yCnt = map.length
+//export class MapDOM {
+//    constructor(map) {
+//        this.xCnt = map[0].length
+//        this.yCnt = map.length
 
-        const tableTag = document.createElement("table")
-        tableTag.setAttribute("class", "grid")
+//        const tableTag = document.createElement("table")
+//        tableTag.setAttribute("class", "grid")
 
-        this._mapSelector = tableTag
-    }
+//        this._mapSelector = tableTag
+//    }
 
-    createMapDOM(map) {
-        document.body.appendChild(this._mapSelector)
+//    createMapDOM(map) {
+//        document.body.appendChild(this._mapSelector)
 
-        for (const row of map) {
-            let rowDOM = document.createElement("tr")
-            for (const cell of row) {
-                const cellDOM = createCellTag(cell)
-                cell.DOM = cellDOM
-                rowDOM.appendChild(cellDOM)
-            }
-            this._mapSelector.appendChild(rowDOM)
-        }
-        this.resizeMapDOM()
-    }
+//        for (const row of map) {
+//            let rowDOM = document.createElement("tr")
+//            for (const cell of row) {
+//                const cellDOM = createCellTag(cell)
+//                cell.DOM = cellDOM
+//                rowDOM.appendChild(cellDOM)
+//            }
+//            this._mapSelector.appendChild(rowDOM)
+//        }
+//        this.resizeMapDOM()
+//    }
  
-    resizeMapDOM() {
-        const headerHeight = getSelectorHeight(".header")
-        const sectionToolHeight = getSelectorHeight(".section")
+//    resizeMapDOM() {
+//        const headerHeight = getSelectorHeight(".header")
+//        const sectionToolHeight = getSelectorHeight(".section")
 
-        this._mapSelector.style.height = this._getResizeSideInPX(document.documentElement.clientHeight - headerHeight - sectionToolHeight, this.xCnt)
-        this._mapSelector.style.width = this._getResizeSideInPX(document.documentElement.clientWidth, this.yCnt)
-    }
+//        this._mapSelector.style.height = this._getResizeSideInPX(document.documentElement.clientHeight - headerHeight - sectionToolHeight, this.xCnt)
+//        this._mapSelector.style.width = this._getResizeSideInPX(document.documentElement.clientWidth, this.yCnt)
+//    }
 
-    _getResizeSideInPX(availableLen, cellCnt) {
-        return getGreatestMultiple(availableLen, cellCnt).toString() + "px"
-    }
+//    _getResizeSideInPX(availableLen, cellCnt) {
+//        return getGreatestMultiple(availableLen, cellCnt).toString() + "px"
+//    }
 
-    getMapSelector() {
-        return this._mapSelector
-    }
+//    getMapSelector() {
+//        return this._mapSelector
+//    }
 
-    addMapObjectEventListener() {
-        for (const row of this._mapSelector.childNodes) {
-            for (const cell of row.childNodes) {
-                cell.addEventListener("addObject", (event) => {
-                    const currCell = event.target 
-                    currCell.className = event["mapObject"].cellClass
-                    currCell.appendChild(event["mapObject"].getDOMView())
-                })
-            }
-        }
-    }
-}
+//    addMapObjectEventListener() {
+//        for (const row of this._mapSelector.childNodes) {
+//            for (const cell of row.childNodes) {
+//                cell.addEventListener("addObject", (event) => {
+//                    const currCell = event.target 
+//                    currCell.className = event["mapObject"].cellClass
+//                    currCell.appendChild(event["mapObject"].getDOMView())
+//                })
+//            }
+//        }
+//    }
+//}
